de71a73729cdea792748171bd472a0133e8d4f40
moxini <jeroennijs@msn.com>
2019-12-05 12:19:09 +0100

    Vraag 1,2 & 3

--- src/main/java/be/pxl/ja/city/City.java
+++ src/main/java/be/pxl/ja/city/City.java
@@ -27,4 +27,20 @@ public class City {
 		dist = dist * 60 * 1.1515 * 1.609344;
 		return dist;
 	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
 }
