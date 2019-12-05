d4142e884f5ea8b49e9cb9b91e2725242f22ff76
JosefOmelyusikPXL <josef.omel1998@hotmail.com>
2019-12-05 10:40:18 +0100

    Vraag 1-2-3

--- src/main/java/be/pxl/ja/city/City.java
+++ src/main/java/be/pxl/ja/city/City.java
@@ -1,15 +1,9 @@
 package be.pxl.ja.city;
 
-import be.pxl.ja.common.DistanceFunction;
-
-public class City implements DistanceFunction {
+public class City {
 	private String name;
 	private double latitude;
 	private double longitude;
-
-	public Double distance(Object city) {
-		return null;
-	}
 
 	public City(String name, double latitude, double longitude) {
 		this.name = name;
@@ -21,9 +15,10 @@ public class City implements DistanceFunction {
 	public String toString() {
 		return name;
 	}
-/*
-		//Source: https://www.geodatasource.com/developers/java
-	    //Calculates the distance between 2 points when given latitude and longitude in decimal degrees
+
+	/*
+		Source: https://www.geodatasource.com/developers/java
+	    Calculates the distance between 2 points when given latitude and longitude in decimal degrees
 
 		double radTheta = Math.toRadians(longitude - other.longitude);
 		double radLatitude = Math.toRadians(latitude);
@@ -32,5 +27,5 @@ public class City implements DistanceFunction {
 		dist = Math.acos(dist);
 		dist = Math.toDegrees(dist);
 		dist = dist * 60 * 1.1515 * 1.609344;
-*/
+	 */
 }
--- src/main/java/be/pxl/ja/city/DistancesBetweenCities.java
+++ src/main/java/be/pxl/ja/city/DistancesBetweenCities.java
@@ -1,5 +1,10 @@
 package be.pxl.ja.city;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class DistancesBetweenCities {
 
 	public static void main(String[] args) {
@@ -7,6 +12,23 @@ public class DistancesBetweenCities {
 		City roermond = new City("Roermond", 51.19417, 5.9875);
 		City maastricht = new City("Maastricht", 50.84833, 5.68889);
 		City aken = new City("Aken", 50.77664, 6.08342);
+		City tienen = new City("Tienen", 50.81057, 4.93622);
+		City maaseik = new City("Maaseik", 51.0983909, 5.7837625);
+
+		TreeSet<String> steden = new TreeSet<>();
+		steden.add(leuven.toString());
+		steden.add(roermond.toString());
+		steden.add(maastricht.toString());
+		steden.add(aken.toString());
+		steden.add(tienen.toString());
+		steden.add(maaseik.toString());
+
+		for (String stad : steden) {
+			System.out.println(stad);
+		}
+
+
+
 	}
 
 }
