9f3d6488a4a3f63636dcd07d984a1d8c793b491e
moxini <jeroennijs@msn.com>
2019-12-05 11:31:23 +0100

    Vraag 1,2 & 3

--- src/test/java/be/pxl/ja/city/CityTest.java
+++ src/test/java/be/pxl/ja/city/CityTest.java
@@ -1,8 +1,26 @@
 package be.pxl.ja.city;
 
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
 public class CityTest {
+    private City tienenSlecht;
+    // Latitude = -90 tot +90
+    // Longitude = -180 tot +180
+    private City maaseik;
 
+    @Before
+    public void init(){
+        tienen = new City("Tienen", 50.81057, 4.93622);
+        maaseik= new City("Maaseik", 51.0983909, 5.7837625);
+    }
 
-    public void
+    @Test
+    pubic void invalidLatitude(){
+        assertThrows(IllegalArgumentException.class, ()-> tienenSlecht.DistanceFunction(tienenSlecht){
+
+        })
+
+    }
 }
